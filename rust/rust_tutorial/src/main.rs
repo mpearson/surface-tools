0a6f7a04386fe0f477bc8045e37135c73a96cae6
Matthew Pearson <matthew.pearson@joby.aero>
2023-11-11 15:41:42 -0800

    add CLI

--- rust/rust_tutorial/src/main.rs
+++ rust/rust_tutorial/src/main.rs
@@ -1,7 +1,18 @@
+use std::env;
+
 mod test1;
 mod test2;
 
 fn main() {
-    test1::guess_numbers();
-    test2::test_search_array();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("No test specified");
+        return;
+    }
+
+    match (&args[1]).trim() {
+        "1" => test1::guess_numbers(),
+        "2" => test2::test_search_array(),
+        _ => println!("No test specified"),
+    }
 }
