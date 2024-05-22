499805b79cbfa5d3a164fd3fb2c5b42973eac3cf
Matthew Pearson <matthew.pearson@joby.aero>
2024-05-21 22:37:47 -0700

    add empy project for bevy stuff

---
+++ rust/bevy_experiment/src/main.rs
@@ -0,0 +1,28 @@
+// use std::env;
+
+// mod test1;
+// mod test2;
+// mod test3;
+// mod test4;
+
+fn main() {
+    println!("wow, such bevy");
+    println!("very webGPU");
+    println!("much borrow check");
+    println!("wow.");
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() < 2 {
+    //     println!("No test specified");
+    //     return;
+    // }
+
+    // let test_number = &args[1];
+
+    // match (test_number).trim() {
+    //     "1" => test1::guess_numbers(),
+    //     "2" => test2::test_search_array(),
+    //     "3" => test3::test_fibonacci(),
+    //     "4" => test4::do_stuff(),
+    //     _ => println!("No test specified"),
+    // }
+}
