f3363bb00d9dcb442c332629bb854abf921d8747
Matthew Pearson <matthew.pearson@joby.aero>
2024-11-03 18:36:18 -0800

    add FPS counter

--- rust/bevy_experiment/src/main.rs
+++ rust/bevy_experiment/src/main.rs
@@ -1,17 +1,24 @@
 // use bevy::ecs::schedule::IntoSystemConfigs;
 // use std::env;
 // use bevy::prelude::*;
+use bevy::prelude::default;
 use bevy::prelude::App;
+use bevy::prelude::Commands;
 use bevy::prelude::DefaultPlugins;
-// use bevy::prelude::Startup;
+use bevy::prelude::Startup;
 // use bevy::prelude::Update;
 
 mod basic_scene;
 mod camera;
-mod orbit_camera;
 mod test_component;
 mod test_plugin;
 mod test_system;
+
+// mod orbit_camera;
+// use crate::orbit_camera::OrbitCameraPlugin;
+
+use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
+use iyes_perf_ui::prelude::*;
 
 fn main() {
     // println!("wow, such bevy");
@@ -22,7 +29,14 @@ fn main() {
     // let systems = (test_system::update_tiles, test_system::print_map_tiles).chain();
 
     App::new()
-        .add_plugins((DefaultPlugins, test_plugin::MyFirstPlugin))
+        .add_plugins((
+            DefaultPlugins,
+            test_plugin::MyFirstPlugin,
+            // orbit_camera::plugin::OrbitCameraPlugin,
+            PerfUiPlugin,
+            FrameTimeDiagnosticsPlugin::default(),
+        ))
+        .add_systems(Startup, setup_fps_counter)
         .run();
 
     // let args: Vec<String> = env::args().collect();
@@ -40,4 +54,22 @@ fn main() {
     //     "4" => test4::do_stuff(),
     //     _ => println!("No test specified"),
     // }
+}
+
+fn setup_fps_counter(mut commands: Commands) {
+    // spawn a camera to be able to see anything
+    // commands.spawn(Camera2dBundle::default());
+
+    // create a simple Perf UI with default settings
+    // and all entries provided by the crate:
+    // commands.spawn(PerfUiCompleteBundle::default());
+    commands.spawn((
+        PerfUiRoot {
+            display_labels: false,
+            layout_horizontal: true,
+            ..default()
+        },
+        // PerfUiEntryFPSWorst::default(),
+        PerfUiEntryFPS::default(),
+    ));
 }
