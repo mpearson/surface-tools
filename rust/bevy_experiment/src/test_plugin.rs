4783145dfb743c972f90f090d5b5b201e6b2774a
Matthew Pearson <matthew.pearson@joby.aero>
2024-11-03 18:36:29 -0800

    WIP camera stuff

--- rust/bevy_experiment/src/test_plugin.rs
+++ rust/bevy_experiment/src/test_plugin.rs
@@ -7,27 +7,33 @@ use bevy::prelude::*;
 // use crate::test_component::PositionGeodetic;
 use crate::basic_scene;
 use crate::camera::{pan_orbit_camera, PanOrbitState};
+
 use crate::test_system;
+
+// use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
+// use iyes_perf_ui::prelude::*;
 
 pub struct MyFirstPlugin;
 
 impl Plugin for MyFirstPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(
-            Startup,
-            (
-                basic_scene::spawn_stuff,
-                test_system::init_map,
-                test_system::update_tiles,
-            ),
-        )
-        .add_systems(
-            Update,
-            (
-                basic_scene::rotate_cube,
-                pan_orbit_camera.run_if(any_with_component::<PanOrbitState>),
-            ),
-        );
+        app
+            // app.add_plugins(FrameTimeDiagnosticsPlugin::default())
+            .add_systems(
+                Startup,
+                (
+                    basic_scene::spawn_stuff,
+                    test_system::init_map,
+                    test_system::update_tiles,
+                ),
+            )
+            .add_systems(
+                Update,
+                (
+                    basic_scene::rotate_cube,
+                    pan_orbit_camera.run_if(any_with_component::<PanOrbitState>),
+                ),
+            );
         // .add_systems(Startup, test_system::print_map_tiles)
         // .add_systems(Update, test_system::print_map_tiles);
     }
