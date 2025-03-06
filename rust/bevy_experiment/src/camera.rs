5048bd86721545bffbdb8201388aa305241ea15f
Matthew Pearson <matthew.pearson@joby.aero>
2025-03-06 10:38:40 -0800

    bring back code for old camera

--- rust/bevy_experiment/src/camera.rs
+++ rust/bevy_experiment/src/camera.rs
@@ -240,13 +240,13 @@ pub fn pan_orbit_camera(
     }
 }
 
-// pub fn spawn_camera(mut commands: Commands) {
-//     let mut camera = PanOrbitCameraBundle::default();
-//     // Position our camera using our component,
-//     // not Transform (it would get overwritten)
-//     camera.state.center = Vec3::new(1.0, 2.0, 3.0);
-//     camera.state.radius = 50.0;
-//     camera.state.pitch = 15.0f32.to_radians();
-//     camera.state.yaw = 30.0f32.to_radians();
-//     commands.spawn(camera);
-// }
+pub fn spawn_camera(mut commands: Commands) {
+    let mut camera = PanOrbitCameraBundle::default();
+    // Position our camera using our component,
+    // not Transform (it would get overwritten)
+    camera.state.center = Vec3::new(1.0, 2.0, 3.0);
+    camera.state.radius = 50.0;
+    camera.state.pitch = 15.0f32.to_radians();
+    camera.state.yaw = 30.0f32.to_radians();
+    commands.spawn(camera);
+}
